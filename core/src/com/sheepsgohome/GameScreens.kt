1816d48f1690c1019b388adc9d0c047459e00861
LittleLightCz <antonin.brettsnajdr@gmail.com>
2017-08-15 14:22:45 +0200

    +GameScreens, -GameObject

---
+++ core/src/com/sheepsgohome/GameScreens.kt
@@ -0,0 +1,11 @@
+package com.sheepsgohome
+
+import com.sheepsgohome.screens.GameplayClassicModeScreen
+import com.sheepsgohome.screens.MainMenuScreen
+import com.sheepsgohome.screens.SplashScreen
+
+object GameScreens {
+    var splashScreen: SplashScreen? = null
+    var mainMenuScreen: MainMenuScreen? = null
+    var gameplayClassicModeScreen: GameplayClassicModeScreen? = null
+}
