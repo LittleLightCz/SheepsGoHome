adbd3b8220d7b9704279db952036fbefdafc9922
LittleLightCz <antonin.brettsnajdr@gmail.com>
2017-08-15 11:24:55 +0200

    SettingsSoundScreen

---
+++ core/src/com/sheepsgohome/screens/SettingsSoundScreen.kt
@@ -0,0 +1,133 @@
+package com.sheepsgohome.screens
+
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.sheepsgohome.GameData
+
+import com.badlogic.gdx.graphics.Texture.TextureFilter
+import com.sheepsgohome.GameData.CAMERA_HEIGHT
+import com.sheepsgohome.GameData.CAMERA_WIDTH
+import com.sheepsgohome.GameData.Loc
+import com.sheepsgohome.GameData.VIRTUAL_JOYSTICK
+import com.sheepsgohome.GameData.SOUND_ENABLED
+import com.sheepsgohome.GameSkins.skin
+
+class SettingsSoundScreen : Screen {
+    private lateinit var stage: Stage
+    private lateinit var table: Table
+
+    private lateinit var buttonSave: TextButton
+    private lateinit var buttonBack: TextButton
+
+    private lateinit var title: Label
+    private lateinit var soundEnabledTitle: Label
+
+    private lateinit var texture: Texture
+    private lateinit var bgImage: Image
+
+    override fun show() {
+        buttonSave = TextButton(Loc.get("save"), skin)
+        buttonBack = TextButton(Loc.get("back"), skin)
+        title = Label(Loc.get("sound"), skin, "menuTitle")
+        soundEnabledTitle = Label(Loc.get("sound.enabled"), skin)
+
+        texture = Texture("menu_background.png")
+        bgImage = Image(texture)
+
+        val soundEnabledSelectBox = SelectBox<String>(skin)
+        soundEnabledSelectBox.setItems(Loc.get("yes"), Loc.get("no"))
+        soundEnabledSelectBox.selectedIndex = if (SOUND_ENABLED) 0 else 1
+
+        val multiplier = 2f
+        stage = Stage(StretchViewport(CAMERA_WIDTH * multiplier, CAMERA_HEIGHT * multiplier))
+
+        table = Table()
+
+        //click listeners
+        buttonSave.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                SOUND_ENABLED = if (soundEnabledSelectBox.selectedIndex == 0) true else false
+                GameData.SavePreferences()
+                (Gdx.app.applicationListener as Game).screen = SettingsScreen()
+            }
+        })
+
+        buttonBack.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                (Gdx.app.applicationListener as Game).screen = SettingsScreen()
+            }
+        })
+
+
+        //table
+        table.setFillParent(true)
+
+        title.setFontScale(GameData.SETTINGS_TITLE_FONT_SCALE)
+        table.add<Label>(title).top().colspan(2).row()
+
+        val contentTable = Table()
+        soundEnabledTitle.setFontScale(GameData.SETTINGS_ITEM_FONT_SCALE)
+        contentTable.add<Label>(soundEnabledTitle).expandX().padRight(2f)
+        contentTable.add(soundEnabledSelectBox).expandX().width(50f).height(20f).row()
+
+
+        table.add(contentTable).expand().colspan(2).top().row()
+
+        table.add<TextButton>(buttonSave).size(BUTTON_SMALL_WIDTH, BUTTON_SMALL_WIDTH / 2).bottom().right()
+        table.add<TextButton>(buttonBack).size(BUTTON_SMALL_WIDTH, BUTTON_SMALL_WIDTH / 2).bottom().left().row()
+
+        stage.addActor(bgImage)
+        stage.addActor(table)
+
+        Gdx.input.inputProcessor = stage
+
+        bgImage.width = CAMERA_WIDTH * multiplier
+        bgImage.height = CAMERA_HEIGHT * multiplier
+        texture.setFilter(TextureFilter.Linear, TextureFilter.Linear)
+    }
+
+    override fun render(delta: Float) {
+        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        stage.act()
+        stage.draw()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
+    override fun pause() {
+
+    }
+
+    override fun resume() {
+
+    }
+
+    override fun hide() {
+        dispose()
+    }
+
+    override fun dispose() {
+        stage.dispose()
+        texture.dispose()
+    }
+
+    companion object {
+        private val BUTTON_WIDTH = 100f
+        private val BUTTON_SMALL_WIDTH = 50f
+    }
+}
