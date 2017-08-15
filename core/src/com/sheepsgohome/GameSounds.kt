b0042ea0b18d645e8252a44ac3ff8cb1ab4e656b
LittleLightCz <antonin.brettsnajdr@gmail.com>
2017-08-15 14:14:44 +0200

    GameSounds

---
+++ core/src/com/sheepsgohome/GameSounds.kt
@@ -0,0 +1,9 @@
+package com.sheepsgohome
+
+import com.badlogic.gdx.audio.Sound
+
+object GameSounds {
+    var soundWolfFailure: Sound? = null
+    var soundSheepSuccess: Sound? = null
+    var soundNewBadge: Sound? = null
+}
