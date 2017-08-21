1158a12af838d5138118c09e8f179e6e68331d17
LittleLightCz <antonin.brettsnajdr@gmail.com>
2017-08-21 13:20:22 +0200

    Finalized MainMenuScreen

--- core/src/com/sheepsgohome/screens/MainMenuScreen.kt
+++ core/src/com/sheepsgohome/screens/MainMenuScreen.kt
@@ -1,12 +1,11 @@
 package com.sheepsgohome.screens
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
+import com.sheepsgohome.gdx.listeners.changed
 import com.sheepsgohome.gdx.listeners.clicked
 import com.sheepsgohome.gdx.screens.switchScreen
 import com.sheepsgohome.gdx.screens.switchToGameplayClassicModeScreen
@@ -33,27 +32,25 @@ class MainMenuScreen : MenuScreen() {
         isChecked = MUSIC_ENABLED
     }
 
-    private val title = Label(loc.get("sheeps.go.home"), skin, "menuTitle")
-    private val version = Label(GameData.VERSION_STRING, skin)
+    private val titleLabel = Label(loc.get("sheeps.go.home"), skin, "menuTitle")
+    private val versionLabel = Label(GameData.VERSION_STRING, skin)
 
     init {
-        musicCheckBox.addListener(object : ChangeListener() {
-            override fun changed(event: ChangeListener.ChangeEvent, actor: Actor) {
-                val cbox = actor as CheckBox?
-                if (cbox != null) {
-                    if (cbox.isChecked) {
-                        MUSIC_ENABLED = true
-                        if (!sheepsTheme.isPlaying) {
-                            sheepsTheme.play()
-                        }
-                    } else {
-                        MUSIC_ENABLED = false
-                        if (sheepsTheme.isPlaying) {
-                            sheepsTheme.pause()
-                        }
+        musicCheckBox.addListener(changed { _, actor ->
+            val cbox = actor as CheckBox?
+            if (cbox != null) {
+                if (cbox.isChecked) {
+                    MUSIC_ENABLED = true
+                    if (!sheepsTheme.isPlaying) {
+                        sheepsTheme.play()
                     }
-                    GameData.savePreferences()
+                } else {
+                    MUSIC_ENABLED = false
+                    if (sheepsTheme.isPlaying) {
+                        sheepsTheme.pause()
+                    }
                 }
+                GameData.savePreferences()
             }
         })
 
@@ -80,14 +77,20 @@ class MainMenuScreen : MenuScreen() {
 
         buttonExit.onClick { Gdx.app.exit() }
 
-        //table
-        table.setFillParent(true)
+        val fontScale = (CAMERA_WIDTH * multiplier - 20) / titleLabel.prefWidth
+        titleLabel.setFontScale(fontScale)
+        table.add(titleLabel)
+                .colspan(2)
+                .height(25f)
+                .padTop(5f)
+                .row()
 
-        val fontScale = (CAMERA_WIDTH * multiplier - 20) / title.prefWidth
-        title.setFontScale(fontScale)
-        table.add<Label>(title).colspan(2).height(25f).padTop(5f).row()
-
-        table.add<Label>(version).expandX().colspan(2).top().center().row()
+        table.add(versionLabel)
+                .expandX()
+                .colspan(2)
+                .top()
+                .center()
+                .row()
 
         val buttonsTable = Table().apply {
             buttonPlay.addTo(this).row()
@@ -96,23 +99,32 @@ class MainMenuScreen : MenuScreen() {
             buttonExit.addTo(this).row()
         }
 
-        table.add(buttonsTable).expandY().colspan(2).row()
+        table.add(buttonsTable)
+                .expandY()
+                .colspan(2)
+                .row()
 
-        val donateSizeScale = 1.65f
+        val checkBoxSizeScale = 1.65f
 
-        val checkBoxSize = BUTTON_WIDTH / (donateSizeScale * 2)
-        table.add<CheckBox>(musicCheckBox).left().bottom().size(checkBoxSize, checkBoxSize).padLeft(2f).padBottom(2f)
+        val checkBoxSize = SheepButton.BUTTON_WIDTH / (checkBoxSizeScale * 2)
+        table.add(musicCheckBox)
+                .left()
+                .bottom()
+                .size(checkBoxSize, checkBoxSize)
+                .padLeft(2f)
+                .padBottom(2f)
 
-        version.setFontScale(0.35f)
-        table.add<ImageButton>(buttonSupport).size(checkBoxSize, checkBoxSize)
-                .bottom().right().padRight(1f).padBottom(2f).row()
+        versionLabel.setFontScale(0.35f)
+        table.add(buttonSupport)
+                .size(checkBoxSize, checkBoxSize)
+                .bottom()
+                .right()
+                .padRight(1f)
+                .padBottom(2f)
+                .row()
 
-
-        stage.addActor(backgroundImage)
-        stage.addActor(table)
 
         Gdx.input.inputProcessor = stage
-
 
         if (ambient.isPlaying) {
             ambient.pause()
@@ -121,9 +133,5 @@ class MainMenuScreen : MenuScreen() {
         if (MUSIC_ENABLED && !sheepsTheme.isPlaying) {
             sheepsTheme.play()
         }
-    }
-
-    companion object {
-        private val BUTTON_WIDTH = 100f
     }
 }
